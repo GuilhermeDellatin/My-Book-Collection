0374840673945f208a5a86a48411eb6669cad198
GuilhermeDellatin <guidellatin@gmail.com>
2020-11-15 18:52:31 -0300

    First commit

---
+++ app/src/main/java/br/com/guilhermedellatin/mybookcollection/BookActivity.kt
@@ -0,0 +1,11 @@
+package br.com.guilhermedellatin.mybookcollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book)
+    }
+}
