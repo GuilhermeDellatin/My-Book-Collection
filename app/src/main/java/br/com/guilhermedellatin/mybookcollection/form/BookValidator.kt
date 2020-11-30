4953f568ed642720119019ef2409e4cafeca1147
GuilhermeDellatin <guidellatin@gmail.com>
2020-11-30 16:58:52 -0300

    Aumento da quantidade de palavras nos campos, correção do titulo

--- app/src/main/java/br/com/guilhermedellatin/mybookcollection/form/BookValidator.kt
+++ app/src/main/java/br/com/guilhermedellatin/mybookcollection/form/BookValidator.kt
@@ -7,7 +7,7 @@ class BookValidator {
         checkName(name) && checkGender(gender) && checkPublishing(publishing)
     }
 
-    private fun checkName(name: String) = name.length in 2..20
-    private fun checkGender(gender: String) = gender.length in 2..20
-    private fun checkPublishing(publishing: String) = publishing.length in 2..20
+    private fun checkName(name: String) = name.length in 2..60
+    private fun checkGender(gender: String) = gender.length in 2..60
+    private fun checkPublishing(publishing: String) = publishing.length in 2..60
 }
